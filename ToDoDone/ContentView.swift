ff9243474934211790243b1d507ee10a0971ef17
Mahmut <sefades@icloud.com>
2024-06-23 13:46:22 +0300

    Initial Commit

---
+++ ToDoDone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoDone
+//
+//  Created by Mahmut Özkan on 23.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoDone/ToDoDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDoneApp.swift
+//  ToDoDone
+//
+//  Created by Mahmut Özkan on 23.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
